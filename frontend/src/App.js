274397f8f11d8012b3c16e306129b8ff44396cef
do-Va <varisdogukan@outlook.com>
2022-03-06 13:51:23 +0300

    Implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
