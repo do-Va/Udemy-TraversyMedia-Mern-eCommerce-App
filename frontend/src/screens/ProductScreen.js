7316391d94b4c92f8e0ec9dd6ad0c2d711a3f6a6
do-Va <varisdogukan@outlook.com>
2022-03-07 14:19:21 +0300

    Fetching products from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 
 import Rating from '../components/Rating';
 
-import products from '../products';
-
 const ProductScreen = () => {
   let params = useParams();
-  const { name, image, rating, numReviews, description, price, countInStock } =
-    products.find(p => p._id === params.id);
+
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, []);
+
+  const {
+    name,
+    image,
+    rating,
+    numReviews,
+    description,
+    price,
+    countInStock,
+  } = product;
 
   return (
     <>
