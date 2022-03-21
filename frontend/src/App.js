8974d776b0734d4484447da0973dcd0b09e7519e
do-Va <varisdogukan@outlook.com>
2022-03-21 17:06:15 +0300

    Added Shipping screen & save address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -18,10 +19,12 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Routes>
+            <Route path="shipping" element={<ShippingScreen />} />
             <Route path="login" element={<LoginScreen />} />
             <Route path="register" element={<RegisterScreen />} />
             <Route path="profile" element={<ProfileScreen />} />
             <Route path="product/:id" element={<ProductScreen />} />
+            <Route path="cart" element={<CartScreen />} />
             <Route path="cart/:id" element={<CartScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
