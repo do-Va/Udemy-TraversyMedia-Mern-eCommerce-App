5a6f16b62e6659d9c3a62bcc0733720d935aac87
do-Va <varisdogukan@outlook.com>
2022-05-07 14:52:27 +0300

    Added the ability to reset user orders and information when logged out.

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,6 +13,7 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_RESET,
+  USER_DETAILS_RESET,
 } from '../constans/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -70,6 +71,9 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
 
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
+
+    case USER_DETAILS_RESET:
+      return { user: {} };
 
     default:
       return state;
