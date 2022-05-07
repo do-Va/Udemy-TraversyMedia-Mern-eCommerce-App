7ebf6c079fa57fbf67a826fe1807670f211044a4
do-Va <varisdogukan@outlook.com>
2022-05-07 15:05:50 +0300

    Added Admin middleware & get users

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -113,4 +113,16 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+
+/**
+ *@desc Get all users
+ *@route Get /api/users/
+ *@access Private/Admin
+ */
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+
+  res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -17,7 +17,6 @@ const protect = expressAsyncHandler(async (req, res, next) => {
       req.user = await User.findById(decoded.id).select('-password');
       next();
     } catch (error) {
-      console.error(error);
       res.status(401);
       throw new Error('Not authorized, token failed');
     }
@@ -29,4 +28,13 @@ const protect = expressAsyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+export { protect, admin };
