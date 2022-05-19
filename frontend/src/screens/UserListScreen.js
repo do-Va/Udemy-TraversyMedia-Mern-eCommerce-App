d8825bc36314a0b5b9801b7ae9920c788c7e6cb2
do-Va <varisdogukan@outlook.com>
2022-05-19 12:44:54 +0300

    Edit user screen and details

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -28,7 +28,7 @@ const UserListScreen = () => {
     } else {
       navigate('/login');
     }
-  }, [dispatch, successDelete]);
+  }, [dispatch, successDelete, userInfo]);
 
   const deleteHandler = id => {
     if (window.confirm('Are you sure')) {
@@ -70,7 +70,7 @@ const UserListScreen = () => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
                     <Button variant="light" className="btn-sm">
                       <i className="fas fa-edit"></i>
                     </Button>
